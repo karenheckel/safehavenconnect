fbae542a13e155f568d7aece1b99f448556a8359
Brianna-Flo <145514791+Brianna-Flo@users.noreply.github.com>
2025-09-29 11:19:47 -0500

    this commit will add the events, organizations, and resources to display previews to the landing page

---
+++ src/components/Organizations.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+const Organizations = () => {
+  return (
+    <Container>
+      <h1>Organizations</h1>
+      <Row>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="https://picsum.photos/180/100" />
+          <Card.Body>
+            <Card.Title>Org 1</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="https://picsum.photos/180/100" />
+          <Card.Body>
+            <Card.Title>Org 2</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="https://picsum.photos/180/100" />
+          <Card.Body>
+            <Card.Title>Org 3</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Row>
+    </Container>
+  );
+};
+
+export default Organizations;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,52 +1,18 @@
 import React from "react";
 import NavBar from "../components/NavBar";
-import Card from "react-bootstrap/Card";
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Events from "../components/Events";
+import Organizations from "../components/Organizations";
+import Resources from "../components/Resources";
+import { Col, Container } from "react-bootstrap";
 
 const Landing = () => {
   return (
-    <>
+    <Container>
       <NavBar />
-      
-      <Container>
-        <h1>Upcoming Events</h1>
-        <Row>
-            
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://picsum.photos/180/100" />
-          <Card.Body>
-            <Card.Title>Event 1</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://picsum.photos/180/100" />
-          <Card.Body>
-            <Card.Title>Event 2</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://picsum.photos/180/100" />
-          <Card.Body>
-            <Card.Title>Event 3</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Row>
-        </Container>
-    </>
+      <Events />
+      <Resources />
+      <Organizations />
+    </Container>
   );
 };
 
