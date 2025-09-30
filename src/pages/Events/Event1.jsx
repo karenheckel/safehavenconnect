e1ca0f4662afd0a99a30fbedd76c625e2ff3d846
Brianna-Flo <145514791+Brianna-Flo@users.noreply.github.com>
2025-09-29 21:39:14 -0500

    this commit will add the event information for the 2nd event instance page

--- src/pages/Events/Event1.jsx
+++ src/pages/Events/Event1.jsx
@@ -1,35 +1,48 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Card, Container, Row, Col, Button } from "react-bootstrap";
+import NavBar from "../../components/NavBar";
 
 const Event1 = () => {
   return (
-    <Container className="my-5">
-      <h1>Volunteer Information Session</h1>
-      <Row className="my-3">
-          <img
-            src="event1.png"
-            alt="SAFE Alliance Logo"
-            className="img-fluid rounded"
-          />
-      </Row>
-      <Row className="my-3">
-          <p>Location: 1515 Grove Blvd, Austin, TX 78741</p>
-          <p>Date: October 1, 2025</p>
-          <p>Time: 6:00 pm - 7:30 pm</p>
-          <p>Event Type: Informational</p>
-          <p>
-            <strong>Organizer:</strong> The SAFE Alliance
-          </p>
-          <Button href="/">Organization Info</Button>
-          <p>
-            <strong>More Info:</strong>{" "}
-            <a href="https://www.safeaustin.org/">SAFE Alliance</a>
-          </p>
-      </Row>
-      <Row className="my-4">
-          <h3>Related Resources</h3>
-      </Row>
-    </Container>
+    <>
+      <NavBar />
+      <Container className="my-5">
+        <h1 className="text-center">Volunteer Information Session</h1>
+        <Row className="my-3 align-items-center">
+          <Col md={6}>
+            <img
+              src="https://www.safeaustin.org/wp-content/uploads/2018/08/fb.png"
+              alt="SAFE Alliance Logo"
+              className="img-fluid rounded"
+            />
+          </Col>
+          <Col className="text-center"  md={6}>
+            <Card body className="shadow-sm">
+              <p>Location: 1515 Grove Blvd, Austin, TX 78741</p>
+              <p>Date: October 1, 2025</p>
+              <p>Time: 6:00 pm - 7:30 pm</p>
+              <p>Event Type: Informational</p>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="my-3">
+          <Col className="text-center" md={6}>
+            <h3>Related Organizations</h3>
+            <p>
+              <strong>Organizer:</strong> The SAFE Alliance
+            </p>
+            <Button href="/">Organization Info</Button>
+            <p>
+              <strong>More Info:</strong>{" "}
+              <a href="https://www.safeaustin.org/">SAFE Alliance</a>
+            </p>
+          </Col>
+          <Col className="text-center" md={6}>
+            <h3>Related Resources</h3>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
