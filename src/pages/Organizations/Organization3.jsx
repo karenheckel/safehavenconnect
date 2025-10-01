4b808a5baa7c236c9ad114c5fe23c41fb0b9a1ce
B4NAN4NA <jonathan.ho.jh.1@gmail.com>
2025-10-01 01:34:37 -0500

    closes #3, Organization3 Page Created

--- src/pages/Organizations/Organization3.jsx
+++ src/pages/Organizations/Organization3.jsx
@@ -7,38 +7,57 @@ const Organization3 = () => {
     <>
       <NavBar />
       <Container className="my-5">
-        <h1 className="text-center">TCFV’s 2025 Texas Town Hall</h1>
+        <h1 className="text-center">Hope Alliance</h1>
         <Row className="my-3 align-items-center">
           <Col md={6}>
             <img
-              src="https://tcfv.org/wp-content/themes/tcfv/assets/img/logo.svg"
-              alt="tcfv logo"
+              src="https://www.hopealliancetx.org/wp-content/uploads/HopeAlliance_Logo_color_tagline-1-300x300.png"
+              alt="Hope Alliance Logo"
               className="img-fluid rounded"
             />
           </Col>
-          <Col className="text-center"  md={6}>
+          <Col className="text-center" md={6}>
             <Card body className="shadow-sm">
-              <p>Location: Texas Tribune Headquarters, Austin</p>
-              <p>Date: October 3, 2025</p>
-              <p>Time: 10:00 am - 12:00 pm</p>
-              <p>Event Type: Panel</p>
+              <p>Location: 1011 Gattis School Rd, Ste 110 Round Rock, TX 78664</p>
+              <p>Services: Emergency Shelter, Counseling, Legal Advocacy</p>
+              <p>
+                Time: <br />
+                Monday 12:00 am - 4:00 pm <br />
+                Tuesday 12:00 am - 4:00 pm <br />
+                Wednesday 12:00 am - 4:00 pm <br />
+                Thursday 12:00 am - 4:00 pm <br />
+                Friday CLOSED <br />
+                Saturday CLOSED <br />
+                Sunday CLOSED
+              </p>
+              <p>Online Availability: Yes</p>
+              <p>
+                Target Demographic: Survivors of Domestic Violence and Sexual
+                Assault
+              </p>
             </Card>
           </Col>
         </Row>
         <Row className="my-3">
           <Col className="text-center" md={6}>
-            <h3>Related Organizations</h3>
-            <p>
-              <strong>Organizer:</strong> tcfv (Texas Council on Family Violence)
-            </p>
-            <Button href="/">Organization Info</Button>
+            <h3>Related Events</h3>
+            <Button href="/">All Organization Events</Button>
             <p>
               <strong>More Info:</strong>{" "}
-              <a href="https://tcfv.org/">tcfv</a>
+              <a href="https://www.hopealliancetx.org/events/">
+                Hope Alliance Events
+              </a>
             </p>
           </Col>
           <Col className="text-center" md={6}>
             <h3>Related Resources</h3>
+            <Button href="/">All Organization Resources</Button>
+            <p>
+              <strong>More Info:</strong>{" "}
+              <a href="https://www.hopealliancetx.org/#">
+                Hope Alliance Resources
+              </a>
+            </p>
           </Col>
         </Row>
       </Container>
