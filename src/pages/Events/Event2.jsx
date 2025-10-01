addefb91673ddddc08f8fac49a416c5c42413ff0
Brianna-Flo <145514791+Brianna-Flo@users.noreply.github.com>
2025-10-01 15:39:40 -0500

    this commit will update the information for the event 2 instance page

--- src/pages/Events/Event2.jsx
+++ src/pages/Events/Event2.jsx
@@ -8,22 +8,28 @@ const Event2 = () => {
       <NavBar />
       <Container className="my-5">
         <h1 className="text-center">
-          Understanding Domestic Abuse and the Impact on Children and Families
+          Hope Alliance Survive. Thrive. Prevent 5K Run/Walk
         </h1>
         <Row className="my-3 align-items-center">
           <Col md={6}>
             <img
-              src="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F1081174993%2F249324158147%2F1%2Foriginal.20250725-115152?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=295%2C0%2C792%2C396&s=50166504b332a1ee1f268b31b16442fa"
+              src="https://www.hopealliancetx.org/wp-content/uploads/HopeAlliance_Logo_color_tagline-1-300x300.png"
               alt="Adele Gladman & Associates Logo"
               className="img-fluid rounded"
             />
           </Col>
           <Col className="text-center" md={6}>
             <Card body className="shadow-sm">
-              <p>Location: Online</p>
-              <p>Date: October 9, 2025</p>
-              <p>Time: 10:30 am - 11:30 am</p>
-              <p>Event Type: Informational</p>
+              <p>
+                Location: San Gabriel Park, 445 E Morrow St, Georgetown, TX
+                78626
+              </p>
+              <p>Date: October 11, 2025</p>
+              <p>Time: 9:00 am - 12:00 pm</p>
+              <p>Event Type: Fundraising</p>
+              <a href="https://runsignup.com/Race/TX/Georgetown/HopeAllianceSurvivetoThrive5K">
+                Event Link
+              </a>
             </Card>
           </Col>
         </Row>
@@ -31,14 +37,12 @@ const Event2 = () => {
           <Col className="text-center" md={6}>
             <h3>Related Organizations</h3>
             <p>
-              <strong>Organizer:</strong> Adele Gladman & Associates
+              <strong>Organizer:</strong> Hope Alliance
             </p>
-            <Button href="/organizations">All Organizations</Button>
+            <Button href="/organization3">Organization Info</Button>
             <p>
               <strong>More Info:</strong>{" "}
-              <a href="https://www.adelegladman.co.uk/">
-                Adele Gladman & Associates
-              </a>
+              <a href="https://www.hopealliancetx.org/">Hope Alliance</a>
             </p>
           </Col>
           <Col className="text-center" md={6}>
@@ -47,10 +51,10 @@ const Event2 = () => {
         </Row>
         <Row>
           <iframe
-            src=""
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27459.248913974126!2d-97.69018351709903!3d30.650690702834712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644d63bd7a7dd71%3A0xa514c5cfeedee4fa!2sSan%20Gabriel%20Park!5e0!3m2!1sen!2sus!4v1759351120144!5m2!1sen!2sus"
             width="600"
             height="450"
-            style={{ border: 0 }}
+            style={{ border:0 }}
             allowfullscreen=""
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
