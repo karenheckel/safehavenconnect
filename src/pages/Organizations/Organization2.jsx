811a5ab712a083edd078b55a424733ca65a568ec
B4NAN4NA <jonathan.ho.jh.1@gmail.com>
2025-10-01 01:26:51 -0500

    Created organization2 page

--- src/pages/Organizations/Organization2.jsx
+++ src/pages/Organizations/Organization2.jsx
@@ -7,39 +7,57 @@ const Organization2 = () => {
     <>
       <NavBar />
       <Container className="my-5">
-        <h1 className="text-center">Understanding Domestic Abuse and the Impact on Children and
-        Families</h1>
+        <h1 className="text-center">The SAFE Alliance</h1>
         <Row className="my-3 align-items-center">
           <Col md={6}>
             <img
-              src="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F1081174993%2F249324158147%2F1%2Foriginal.20250725-115152?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=295%2C0%2C792%2C396&s=50166504b332a1ee1f268b31b16442fa"
-              alt="Adele Gladman & Associates Logo"
+              src="https://www.safeaustin.org/wp-content/uploads/2018/08/fb.png"
+              alt="The SAFE Alliance Logo"
               className="img-fluid rounded"
             />
           </Col>
-          <Col className="text-center"  md={6}>
+          <Col className="text-center" md={6}>
             <Card body className="shadow-sm">
-              <p>Location: Online</p>
-              <p>Date: October 9, 2025</p>
-              <p>Time: 10:30 am - 11:30 am</p>
-              <p>Event Type: Informational</p>
+              <p>Location: 1515 Grove Blvd, Austin, TX 78741</p>
+              <p>Services: Emergency Shelter, Counseling, Legal Advocacy</p>
+              <p>
+                Time: <br />
+                Monday 8:00 am - 4:00 pm <br />
+                Tuesday 8:00 am - 4:00 pm <br />
+                Wednesday 8:00 am - 4:00 pm <br />
+                Thursday 8:00 am - 4:00 pm <br />
+                Friday 8:00 am - 4:00 pm <br />
+                Saturday CLOSED <br />
+                Sunday CLOSED
+              </p>
+              <p>Online Availability: Yes</p>
+              <p>
+                Target Demographic: Survivors of Domestic Violence, Sexual
+                Assault, and Human Trafficking
+              </p>
             </Card>
           </Col>
         </Row>
         <Row className="my-3">
           <Col className="text-center" md={6}>
-            <h3>Related Organizations</h3>
-            <p>
-              <strong>Organizer:</strong> Adele Gladman & Associates
-            </p>
-            <Button href="/">Organization Info</Button>
+            <h3>Related Events</h3>
+            <Button href="/">All Organization Events</Button>
             <p>
               <strong>More Info:</strong>{" "}
-              <a href="https://www.adelegladman.co.uk/">Adele Gladman & Associates</a>
+              <a href="https://www.safeaustin.org/get-involved/donate/events/">
+                The SAFE Alliance Events
+              </a>
             </p>
           </Col>
           <Col className="text-center" md={6}>
             <h3>Related Resources</h3>
+            <Button href="/">All Organization Resources</Button>
+            <p>
+              <strong>More Info:</strong>{" "}
+              <a href="https://www.safeaustin.org/our-services/">
+                The SAFE Alliance Resources
+              </a>
+            </p>
           </Col>
         </Row>
       </Container>
