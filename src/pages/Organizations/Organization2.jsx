c29fc167f1369cb33769fe025886974353ce7f23
parul-sadasivuni <txpsree@gmail.com>
2025-10-01 19:55:03 -0500

    organizations fix

--- src/pages/Organizations/Organization2.jsx
+++ src/pages/Organizations/Organization2.jsx
@@ -7,33 +7,33 @@ const Organization2 = () => {
     <>
       <NavBar />
       <Container className="my-5">
-        <h1 className="text-center">The SAFE Alliance</h1>
+        <h1 className="text-center">Hope Alliance</h1>
         <Row className="my-3 align-items-center">
           <Col md={6}>
             <img
-              src="https://www.safeaustin.org/wp-content/uploads/2018/08/fb.png"
-              alt="The SAFE Alliance Logo"
+              src="https://www.hopealliancetx.org/wp-content/uploads/HopeAlliance_Logo_color_tagline-1-300x300.png"
+              alt="Hope Alliance Logo"
               className="img-fluid rounded"
             />
           </Col>
           <Col className="text-center" md={6}>
             <Card body className="shadow-sm">
-              <p>Location: 1515 Grove Blvd, Austin, TX 78741</p>
+              <p>Location: 1011 Gattis School Rd, Ste 110 Round Rock, TX 78664</p>
               <p>Services: Emergency Shelter, Counseling, Legal Advocacy</p>
               <p>
                 Time: <br />
-                Monday 8:00 am - 4:00 pm <br />
-                Tuesday 8:00 am - 4:00 pm <br />
-                Wednesday 8:00 am - 4:00 pm <br />
-                Thursday 8:00 am - 4:00 pm <br />
-                Friday 8:00 am - 4:00 pm <br />
+                Monday 12:00 am - 4:00 pm <br />
+                Tuesday 12:00 am - 4:00 pm <br />
+                Wednesday 12:00 am - 4:00 pm <br />
+                Thursday 12:00 am - 4:00 pm <br />
+                Friday CLOSED <br />
                 Saturday CLOSED <br />
                 Sunday CLOSED
               </p>
               <p>Online Availability: Yes</p>
               <p>
-                Target Demographic: Survivors of Domestic Violence, Sexual
-                Assault, and Human Trafficking
+                Target Demographic: Survivors of Domestic Violence and Sexual
+                Assault
               </p>
             </Card>
           </Col>
@@ -42,21 +42,21 @@ const Organization2 = () => {
           <Col className="text-center" md={6}>
             <h3>Related Events</h3>
             <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="https://www.safeaustin.org/wp-content/uploads/2018/08/fb.png" />
+              <Card.Img variant="top" src="https://www.hopealliancetx.org/wp-content/uploads/HopeAlliance_Logo_color_tagline-1-300x300.png" />
               <Card.Body>
-                <Card.Title>Volunteer Information Session</Card.Title>
-                <Card.Text>Location: 1515 Grove Blvd, Austin, TX 78741</Card.Text>
-                <Card.Text>Time: 6:00 pm - 7:30 pm</Card.Text>
-                <Card.Text>Date: October 1, 2025</Card.Text>
-                <Card.Text>Event Type: Informational</Card.Text>
-                <Card.Text>Organization: The SAFE Alliance</Card.Text>
+                <Card.Title>Hope Alliance Survive. Thrive. Prevent 5K Run/Walk</Card.Title>
+                <Card.Text>Location: 445 E Morrow St, Georgetown, TX 78626 (San Gabriel Park)</Card.Text>
+                <Card.Text>Time: 9:00 am - 12:00 pm</Card.Text>
+                <Card.Text>Date: October 11, 2025</Card.Text>
+                <Card.Text>Event Type: Fundraising</Card.Text>
+                <Card.Text>Organization: Hope Alliance</Card.Text>
                 <Button
                   style={{
                     color: "black",
                     backgroundColor: "#cde5d7",
                     borderColor: "black",
                   }}
-                  href="/event1"
+                  href="/event2"
                 >
                   View Event
                 </Button>
@@ -68,8 +68,8 @@ const Organization2 = () => {
             <Button href="/">All Organization Resources</Button>
             <p>
               <strong>More Info:</strong>{" "}
-              <a href="https://www.safeaustin.org/our-services/">
-                The SAFE Alliance Resources
+              <a href="https://www.hopealliancetx.org/#">
+                Hope Alliance Resources
               </a>
             </p>
           </Col>
