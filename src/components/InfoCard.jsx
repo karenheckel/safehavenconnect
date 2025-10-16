eb938317a7503b00a86bcf3dc208f20208c39e07
Brianna-Flo <145514791+Brianna-Flo@users.noreply.github.com>
2025-10-15 21:30:36 -0500

    this commit will create a reusable card component that can be used across event, organization, and resource cards

---
+++ src/components/InfoCard.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+
+const InfoCard = ({ cardType, cardInfo }) => {
+    const {
+        title,
+        imgUrl,
+        location,
+        hours,
+        onlineAvailability,
+        organization,
+        services,
+        targetDemographic,
+        time,
+        date,
+        eventType,
+        resourceType,
+        pageLink,
+      } = cardInfo;
+
+      const infoToPresent = {
+        resource: [
+          { label: "Location", value: location },
+          { label: "Type", value: resourceType },
+          { label: "Hours", value: hours },
+          { label: "Online Availability", value: onlineAvailability },
+          { label: "Organization", value: organization },
+        ],
+        organization: [
+          { label: "Location", value: location },
+          { label: "Services", value: services },
+          { label: "Hours", value: hours },
+          { label: "Online Availability", value: onlineAvailability },
+          { label: "Target Demographic", value: targetDemographic },
+        ],
+        event: [
+          { label: "Location", value: location },
+          { label: "Time", value: time },
+          { label: "Date", value: date },
+          { label: "Event Type", value: eventType },
+          { label: "Organization", value: organization },
+        ],
+      };
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={imgUrl} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        {infoToPresent[cardType].map(
+            (info, index) => (
+                <Card.text key={index}>
+                    {info.label}: {info.value}
+                </Card.text>
+            )
+        )}
+        <Button
+          style={{
+            color: "black",
+            backgroundColor: "#cde5d7",
+            borderColor: "black",
+          }}
+          href={pageLink}
+        >
+          View {cardType.charAt(0).toUpperCase() + cardType.slice(1)}
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default InfoCard;
