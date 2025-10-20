5d18d7de9085ce2c9d6345b6efa5c536076fa6fd
Brianna-Flo <145514791+Brianna-Flo@users.noreply.github.com>
2025-10-19 23:44:51 -0500

    this commit will modify the events page to dynamically render event cards by pulling from backend database Close #49

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,42 +1,49 @@
 import React from "react";
-import { Container, Row, Card, Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
 import NavigationBar from "../components/NavigationBar";
+import InfoCard from "../components/InfoCard";
+import axios from "axios";
+
+const DATABASE_URL = "http://localhost:5001";
 
 const Events = () => {
-  const eventsInfo = [
-    {
-      title: "Volunteer Information Session",
-      location: "1515 Grove Blvd, Austin, TX 78741",
-      time: "6:00 pm - 7:30 pm",
-      date: "October 1, 2025",
-      eventType: "Informational",
-      organization: "The SAFE Alliance",
-      imgUrl: "https://www.safeaustin.org/wp-content/uploads/2018/08/fb.png",
-      pageLink: "/event1",
-    },
-    {
-      title:
-        "Hope Alliance Survive. Thrive. Prevent 5K Run/Walk",
-      location: "445 E Morrow St, Georgetown, TX 78626 (San Gabriel Park)",
-      time: "9:00 am - 12:00 pm",
-      date: "October 11, 2025",
-      eventType: "Fundraising",
-      organization: "Hope Alliance",
-      imgUrl:
-        "https://www.hopealliancetx.org/wp-content/uploads/HopeAlliance_Logo_color_tagline-1-300x300.png",
-      pageLink: "/event2",
-    },
-    {
-      title: "TCFV’s 2025 Texas Town Hall",
-      location: "Texas Tribune Headquarters, Austin, TX",
-      time: "10:00 am - 12:00 pm",
-      date: "October 3, 2025",
-      eventType: "Panel",
-      organization: "Texas Council on Family Violence",
-      imgUrl: "https://tcfv.org/wp-content/themes/tcfv/assets/img/logo.svg",
-      pageLink: "/event3",
-    },
-  ];
+  const [eventsInfo, setEventsInfo] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getEvents = async () => {
+      try {
+        const res = await axios.get(DATABASE_URL, "/api/events");
+        const formatEvents = res.data.map((event) => ({
+          title: event.name,
+          location: event.location,
+          time: event.start_time,
+          date: event.date,
+          eventType: event.event_type,
+          organization: event.relatedOrganizations[0].title,
+          imgUrl: event.image_url,
+          pageLink: event.event_url,
+        }));
+        setEventsInfo(formatEvents);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching events:", error);
+        setLoading(false);
+      }
+    };
+    getEvents();
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <NavigationBar />
+        <p>Loading events</p>
+      </>
+    );
+  }
+
   return (
     <>
       <NavigationBar />
@@ -44,28 +51,8 @@ const Events = () => {
         <h1>Upcoming Events</h1>
         <p>Number of events: {eventsInfo.length}</p>
         <Row className="justify-content-center">
-          {eventsInfo.map((eventInfo) => (
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={eventInfo.imgUrl} />
-              <Card.Body>
-                <Card.Title>{eventInfo.title}</Card.Title>
-                <Card.Text>Location: {eventInfo.location}</Card.Text>
-                <Card.Text>Time: {eventInfo.time}</Card.Text>
-                <Card.Text>Date: {eventInfo.date}</Card.Text>
-                <Card.Text>Event Type: {eventInfo.eventType}</Card.Text>
-                <Card.Text>Organization: {eventInfo.organization}</Card.Text>
-                <Button
-                  style={{
-                    color: "black",
-                    backgroundColor: "#cde5d7",
-                    borderColor: "black",
-                  }}
-                  href={eventInfo.pageLink}
-                >
-                  View Event
-                </Button>
-              </Card.Body>
-            </Card>
+          {eventsInfo.map((eventInfo, index) => (
+            <InfoCard key={index} cardType="event" cardInfo={eventInfo} />
           ))}
         </Row>
       </Container>
