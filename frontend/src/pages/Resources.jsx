828bd888bfe7f3975aa2df88fb52cfed0c46aed1
Brianna-Flo <145514791+Brianna-Flo@users.noreply.github.com>
2025-10-25 22:51:44 -0500

    this commit will add hardcoded backup data for resources

--- frontend/src/pages/Resources.jsx
+++ frontend/src/pages/Resources.jsx
@@ -6,6 +6,43 @@ import axios from "axios";
 
 const BACKEND_URL = "https://backend.safehavenconnect.me";
 
+const backupResources = [
+  {
+    id: "default0",
+    title: "Kelly White Family Shelter",
+    location: "4800 Manor Rd A, Austin, TX 78723",
+    resource_type: "Shelter/Housing",
+    hours: "24/7",
+    organization: "The SAFE Alliance",
+    online_availability: "No",
+    image_url: "https://www.safeaustin.org/wp-content/uploads/2018/08/fb.png",
+    pageLink: "/resource1",
+  },
+  {
+    id: "default1",
+    title: "24 Hour HOPELine",
+    location: "1-800-460-7233",
+    resource_type: "Crisis Hotline",
+    hours: "24/7",
+    organization: "Hope Alliance",
+    online_availability: "Yes",
+    image_url:
+      "https://www.hopealliancetx.org/wp-content/uploads/HopeAlliance_Logo_color_tagline-1-300x300.png",
+    pageLink: "/resource2",
+  },
+  {
+    id: "default2",
+    title: "Domestic Violence Awareness Month Resources",
+    location: "https://tcfv.org/awareness/",
+    resource_type: "Informational",
+    hours: "N/A",
+    organization: "Texas Council on Family Violence",
+    online_availability: "Yes",
+    image_url: "https://tcfv.org/wp-content/themes/tcfv/assets/img/logo.svg",
+    pageLink: "/resource3",
+  },
+];
+
 const Resources = () => {
   const [resources, setResources] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -27,9 +64,14 @@ const Resources = () => {
           imgUrl: resource.image_url,
           pageLink: resource.website_url,
         }));
-        setResources(formatResources);
+        if (formatResources.length === 0) {
+          setResources(backupResources);
+        } else {
+          setResources(formatResources);
+        }
       } catch (error) {
         console.error("Error fetching resources:", error);
+        setResources(backupResources);
       } finally {
         setLoading(false);
       }
